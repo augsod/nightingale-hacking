170c31f6529ca955b3212defb0ecced8f6eed31c
August Sodora <augsod@gmail.com>
2012-03-22 20:39:48 -0400

    Update diff-build script to produce lots less noise

--- diff-build.sh
+++ diff-build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-diff -rq compiled ccompiled -x Makefile |
-grep -v cmake |
-grep -v makefile |
-grep -v "\.o" |
-grep -v "\.so"
+
+# just xpt and .a for now because it produces a lot of noise
+
+diff -rq compiled ccompiled -x Makefile \
+| grep -v cmake | grep -v CMakeFiles \
+| grep -v "config.log" | grep -v "config.status" | grep -v "dependencies" \
+| grep -v "\.o" | grep -v "\.so" \
+| grep -v "xpt" | grep -v "\.a"
