deebf2605c539f24e32f499dd8f1f552c86db732
August Sodora <augsod@gmail.com>
2012-03-26 17:34:20 -0400

    cmake: First attempt at building some of these preprocessed inis

--- cmake/Modules/SbDistribute.cmake
+++ cmake/Modules/SbDistribute.cmake
@@ -8,3 +8,29 @@ macro(SB_DISTRIBUTE)
       ${CMAKE_BINARY_DIR}/dist/${src})
   endforeach()
 endmacro()
+
+macro(SB_DISTRIBUTE_PREPROCESS)
+  #TODO: Define these somewhere else
+  set(definitions
+    -DSB_APPNAME=Nightingale
+    -DSB_MILESTONE=1.11.0b2
+    -DSB_BUILD_ID=20120314113350
+    -DSB_BUILD_NUMBER=2237
+    -DSB_CRASHREPORT_SERVER_URL=https://crashreports.songbirdnest.com/submit)
+
+  set(preprocessor ${CMAKE_SOURCE_DIR}/dependencies)
+  if(UNIX)
+    set(preprocessor ${preprocessor}/linux-i686)
+  endif()
+  set(preprocessor
+    ${preprocessor}/mozilla-1.9.2/release/scripts/preprocessor.pl)
+
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/dist)
+  foreach(src ${ARGN})
+    set(cmd perl ${preprocessor} ${definitions}
+      ${CMAKE_CURRENT_SOURCE_DIR}/${src}.in > ${CMAKE_BINARY_DIR}/dist/${src})
+
+    add_custom_target(${src} ALL
+      COMMAND ${cmd})
+  endforeach()
+endmacro()
