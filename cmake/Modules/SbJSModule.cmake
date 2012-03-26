f375cf4738f92ad71a8b7c489205c14f724ae542
August Sodora <augsod@gmail.com>
2012-03-26 00:33:09 -0400

    cmake: Build components/albumart/src jsmodules

--- cmake/Modules/SbJSModule.cmake
+++ cmake/Modules/SbJSModule.cmake
@@ -1,10 +1,10 @@
-macro(SB_JS_COMPONENT)
+macro(SB_JS_MODULE)
   # When we start linking more of this together, see if we can just put this
   # all in ${CMAKE_CURRENT_BINARY_DIR}
-  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/dist/components)
+  file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/dist/jsmodules)
   foreach(src ${ARGN})
     configure_file(
       ${CMAKE_CURRENT_SOURCE_DIR}/${src}
-      ${CMAKE_BINARY_DIR}/dist/components/${src})
+      ${CMAKE_BINARY_DIR}/dist/jsmodules/${src})
   endforeach()
 endmacro()
