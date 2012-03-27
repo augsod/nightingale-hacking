2ec82f4d148fa614a9c4e4454d6c78f353bf040b
August Sodora <augsod@gmail.com>
2012-03-26 20:58:51 -0400

    cmake: Filter out some more cmake build progress for a while

--- diff-build.sh
+++ diff-build.sh
@@ -3,7 +3,9 @@
 # just xpt and .a for now because it produces a lot of noise
 
 diff -rq compiled ccompiled -x Makefile \
-| grep -v cmake | grep -v CMakeFiles \
+| grep -v cmake | grep -v CMake \
 | grep -v "config.log" | grep -v "config.status" | grep -v "dependencies" \
 | grep -v "\.o" | grep -v "\.so" \
-| grep -v "xpt" | grep -v "\.a"
+| grep -v "xpt" | grep -v "\.a" \
+| grep -v "test" | grep -v "mock" \
+| grep -v "win32" | grep -v "macosx" | grep -v "windows"
